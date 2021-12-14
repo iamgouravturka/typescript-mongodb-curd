97b683224157607087a032a6185a9fe70bc0a599
maurya008 <mauryavinay008@gmail.com>
2021-12-14 09:59:38 +0530

    done

--- src/controller/product.ts
+++ src/controller/product.ts
@@ -4,30 +4,28 @@ import { Product as ProdcutStructure } from '../interface/productInf';
 import Product  from "../model/product";
 
 export const getProductList = async (req:any, res:any) => {
-    const data:any = await Product.find().populate('user');
-    res.send(data);
-  };
+  const data:any = await Product.find().populate('user');
+  res.send(data);
+};
 
 // Get product by ID
 export const getProduct = async (req:express.Request, res:express.Response) => {
-    const product: any = req.params;
+  const product: any = req.params;
     try {
-        if(!product.id)
-            return res.status(500).send("Error")
-        const data:any = await Product.findById({_id: product.id}).populate('user')
-        res.send(data);
-
-            
-    } catch (error) {
-       console.error("error");
-    }
-  };
+      if(!product.id)
+        return res.status(500).send("Error")
+      const data:any = await Product.findById({_id: product.id}).populate('user')
+      res.send(data);        
+  } catch (error) {
+     console.error("error");
+  }
+};
 
   // Create product
   export const createProduct = async (req:any, res:any) => {
     const request: ProdcutStructure = req.body;
-    let prod = new Product(request);
-    prod.save((err:any, result:any) => {
+    let product = new Product(request);
+    product.save((err:any, result:any) => {
       if (err) {
         res.send("Error!");
       } else {
@@ -39,28 +37,28 @@ export const getProduct = async (req:express.Request, res:express.Response) => {
 
 //Update product by ID 
 export const updateProduct = async (req:express.Request, res:express.Response) => {
-    const product: any = req.params;
-    const productBody: ProdcutStructure = req.body;
-    try {
-      if (!product.id)
-        return res.status(500).send("Error")
-      const data:any = await Product.findByIdAndUpdate({_id: product.id});
-      if(productBody.productName) data.productName = productBody.productName;
-      if(productBody.productCode) data.productCode = productBody.productCode;
+  const product: any = req.params;
+  const productBody: ProdcutStructure = req.body;
+  try {
+    if (!product.id)
+      return res.status(500).send("Error")
+    const data:any = await Product.findByIdAndUpdate({_id: product.id});
+    if(productBody.productName) data.productName = productBody.productName;
+    if(productBody.productCode) data.productCode = productBody.productCode;
     
-      const result = await data.save();
-      res.send(result);
-    } catch (error) {
-        console.error("Error");
-    }     
-  };
+    const result = await data.save();
+    res.send(result);
+  } catch (error) {
+      console.error("Error");
+  }     
+};
 
   // Delete Product By ID
-  export const deleteProduct = async (req:any, res:any) => {
-    const product: ProdcutStructure = req.params;
+export const deleteProduct = async (req:any, res:any) => {
+  const product: ProdcutStructure = req.params;
 
-    const result:any = await Product.findOneAndDelete({_id: product.id});
+  const result:any = await Product.findOneAndDelete({_id: product.id});
 
-    res.send(result);
-  };
+  res.send(result);
+};
     
--- src/model/product.ts
+++ src/model/product.ts
@@ -3,17 +3,17 @@ import {
   } from 'mongoose';
   import mongoose from 'mongoose';
   
- const ProductSchema = new Schema({
-    id: { type: mongoose.Schema.Types.ObjectId, required: false },
-    productName: String,
-    productCode: String,
-    proddescription: String,
-    prodRating: Number,
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user"
+const ProductSchema = new Schema({
+  id: { type: mongoose.Schema.Types.ObjectId, required: false },
+  productName: String,
+  productCode: String,
+  proddescription: String,
+  prodRating: Number,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
   },
-  });
+});
 
 const Product = mongoose.model("Product", ProductSchema);
 export default Product;
